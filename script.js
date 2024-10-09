8a4496118d90d18836ed2dd55101c9033f19ac5d
SpencerCragun <spencer.cragun@gmail.com>
2024-10-09 10:38:28 -0700

    create html and js files, complete block-07  task

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+// create a string, saying "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+const alertMessage = `You have received this message because you have been chosen to open an important vault. Here is the secret combination:`;
+
+// create 3 variables, each resulting in one of the numbers needed for the secret code. 10, 40, and 39.
+// use three different arithmetic operations in each variable. 
+
+//variable 1 will output 10
+const firstAnswer = 2*5;
+//variable 2 will output 40
+const secondAnswer = 80/2;
+//variable 3 will out put 39
+const thirdAnswer = 20+19;
+
+alert(alertMessage + firstAnswer + `-` + secondAnswer + `-` + thirdAnswer);
+
+
+
+
